df7d35389545fbe5f3f3185bba923094c8ccdace
Jay Lin <weijingjaylin@gmail.com>
2016-01-30 03:11:55 -0800

    update path

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
     define('APP_PATH','./app/');
     define('APP_DEBUG', true);
-    require_once('C:\www\_fmk\thinkphp\3.2.3\ThinkPHP.php');
+    require_once('../../_fmk/thinkphp/3.2.3/ThinkPHP.php');
